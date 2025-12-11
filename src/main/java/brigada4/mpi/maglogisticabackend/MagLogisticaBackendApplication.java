80e878590df7374cc5086d410225dcba0dbac34a
SashaLyapanov <lyapanov_s@mail.ru>
2025-12-11 12:14:20 +0300

    Init commit

---
+++ src/main/java/brigada4/mpi/maglogisticabackend/MagLogisticaBackendApplication.java
@@ -0,0 +1,13 @@
+package brigada4.mpi.maglogisticabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagLogisticaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MagLogisticaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/brigada4/mpi/maglogisticabackend/MagLogisticaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package brigada4.mpi.maglogisticabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagLogisticaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
