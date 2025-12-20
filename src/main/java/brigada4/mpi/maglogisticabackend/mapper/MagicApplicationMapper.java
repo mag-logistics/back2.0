7bc3f73e3cbb30ea7cd3809a86e7e00af25e30e0
SashaLyapanov <lyapanov_s@mail.ru>
2025-12-20 15:44:58 +0300

    Update parent project version. Add MagicAppPattern model.

---
+++ src/main/java/brigada4/mpi/maglogisticabackend/mapper/MagicApplicationMapper.java
@@ -0,0 +1,33 @@
+package brigada4.mpi.maglogisticabackend.mapper;
+
+import brigada4.mpi.maglogisticabackend.dto.MagicAppPatternDTO;
+import brigada4.mpi.maglogisticabackend.dto.MagicApplicationDTO;
+import brigada4.mpi.maglogisticabackend.models.MagicAppPattern;
+import brigada4.mpi.maglogisticabackend.models.MagicApplication;
+import org.mapstruct.Mapper;
+
+import java.util.List;
+
+@Mapper(componentModel = "spring")
+public interface MagicApplicationMapper {
+
+    MagicApplication fromMagicApplicationDTO(MagicApplicationDTO magicApplicationDTO);
+
+    MagicApplicationDTO fromMagicApplication(MagicApplication magicApplication);
+
+    List<MagicApplicationDTO> fromMagicApplications(List<MagicApplication> magicApplications);
+
+    List<MagicApplication> fromMagicApplicationDTOs(List<MagicApplicationDTO> magicApplicationDTOS);
+
+    MagicAppPattern fromMagicApp(MagicApplication magicApplication);
+
+    MagicApplication fromPattern(MagicAppPattern magicAppPattern);
+
+    MagicAppPattern fromMagicAppDTO(MagicApplicationDTO magicApplication);
+
+    MagicApplication fromPatternDTO(MagicAppPatternDTO magicAppPattern);
+
+    MagicApplicationDTO DTOFromPatternDTO(MagicAppPatternDTO magicAppPattern);
+
+    MagicApplicationDTO DTOFromPattern(MagicAppPattern magicAppPattern);
+}
---
+++ src/main/java/brigada4/mpi/maglogisticabackend/models/MagicAppPattern.java
@@ -0,0 +1,30 @@
+package brigada4.mpi.maglogisticabackend.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import lombok.Data;
+
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "magic_app_patterns")
+public class MagicAppPattern extends GenericEntity{
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "magician_id", referencedColumnName = "id")
+    private Magician magician;
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "magic_id", referencedColumnName = "id")
+    private Magic magic;
+
+    @NotEmpty
+    @Column(name = "volume")
+    private int volume;
+
+    @NotEmpty
+    @Column(name = "deadline")
+    private Date deadline;
+
+}
