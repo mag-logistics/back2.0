a3283869ad0d46e0eabbc058607333c9b8d6d4bb
Vladislav <vladislav.tezev@gmail.com>
2025-12-27 21:40:00 +0300

    Add payload for creating request to hunter application
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

--- src/main/java/brigada4/mpi/maglogisticabackend/payload/CreateHunterApplicationRequest.java
+++ src/main/java/brigada4/mpi/maglogisticabackend/payload/CreateHunterApplicationRequest.java
@@ -3,9 +3,7 @@ package brigada4.mpi.maglogisticabackend.payload;
 import java.util.Date;
 
 public record CreateHunterApplicationRequest(
-        String hunterResponseId,
         String magicId,
         int volume,
-        Date initDate,
         Date deadline
 ) {}
