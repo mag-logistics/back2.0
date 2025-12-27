b1aa53cc6e2aac327145acffb1e92429bca02fa9
Vladislav <vladislav.tezev@gmail.com>
2025-12-27 21:38:29 +0300

    Update GlobalHandlerException
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

--- src/main/java/brigada4/mpi/maglogisticabackend/exception/GlobalExceptionHandler.java
+++ src/main/java/brigada4/mpi/maglogisticabackend/exception/GlobalExceptionHandler.java
@@ -2,7 +2,6 @@ package brigada4.mpi.maglogisticabackend.exception;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.data.crossstore.ChangeSetPersister;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
