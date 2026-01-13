8261621a32243a7b54ee7251d208e6627b7cf30f
Vladislav <vladislav.tezev@gmail.com>
2026-01-13 23:32:54 +0300

    Full implementation funcionality for hunter
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

---
+++ src/main/java/brigada4/mpi/maglogisticabackend/mapper/HunterResponseMapper.java
@@ -0,0 +1,11 @@
+package brigada4.mpi.maglogisticabackend.mapper;
+
+import brigada4.mpi.maglogisticabackend.dto.HunterResponseDTO;
+import brigada4.mpi.maglogisticabackend.models.HunterResponse;
+import org.mapstruct.Mapper;
+
+@Mapper
+public interface HunterResponseMapper {
+    HunterResponseDTO toDTO(HunterResponse hunterResponse);
+    HunterResponse toEntity(HunterResponseDTO hunterResponseDTO);
+}
--- src/main/java/brigada4/mpi/maglogisticabackend/repositories/HunterResponseRepository.java
+++ src/main/java/brigada4/mpi/maglogisticabackend/repositories/HunterResponseRepository.java
@@ -4,6 +4,9 @@ import brigada4.mpi.maglogisticabackend.models.HunterResponse;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface HunterResponseRepository extends JpaRepository<HunterResponse, String> {
+    List<HunterResponse> findAllByHunterId(String hunterId);
 }
