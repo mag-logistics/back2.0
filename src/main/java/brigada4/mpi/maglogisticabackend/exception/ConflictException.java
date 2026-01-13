19a9436508856a915a1ea1da48610df96f9103e7
Vladislav <vladislav.tezev@gmail.com>
2026-01-13 21:53:56 +0300

    Add new custom exception
    Signed-off-by: Vladislav <vladislav.tezev@gmail.com>

---
+++ src/main/java/brigada4/mpi/maglogisticabackend/exception/ConflictException.java
@@ -0,0 +1,7 @@
+package brigada4.mpi.maglogisticabackend.exception;
+
+public class ConflictException extends RuntimeException {
+    public ConflictException(String message) {
+        super(message);
+    }
+}
